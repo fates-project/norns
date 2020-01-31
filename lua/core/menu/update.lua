a31508f9fc667feeca75e2e3b5d7c9926f07a4b8
okyeron <1909661+okyeron@users.noreply.github.com>
2020-01-31 12:25:13 -0600

    Revert "fates tweaks"
    This reverts commit cc0c016b0c19215e7df39d0a25019431eff999b0.

--- lua/core/menu/update.lua
+++ lua/core/menu/update.lua
@@ -8,12 +8,17 @@ local function get_update_2() end
 
 local function check_newest()
 <<<<<<< HEAD
+<<<<<<< HEAD
   print("checking for update")
   norns.system_cmd( [[curl -s \
       https://api.github.com/repos/fates-project/norns/releases/latest \
 =======
   print("must update manually")
   -- norns.system_cmd( [[curl -s \
+=======
+  print("checking for update")
+  norns.system_cmd( [[curl -s \
+>>>>>>> Revert "fates tweaks"
       https://api.github.com/repos/monome/norns/releases/latest \
 >>>>>>> fates tweaks
       | grep "browser_download_url.*" \
@@ -98,7 +103,7 @@ m.key = function(n,z)
       _menu.redraw()
     elseif n==3 and z==1 then
       m.stage="update"
-      --get_update()
+      get_update()
     end
   elseif m.stage=="update" then
     if n==2 and z==1 then
